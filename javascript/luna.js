2e8e8294041bd80336a2884480582158c4e8103f
Andromeda08 <vkovacsbalazs26@gmail.com>
2021-02-24 14:50:03 +0100

    version 0.1.4

--- javascript/luna.js
+++ javascript/luna.js
@@ -1,8 +1,13 @@
-const lunaVersion = 'v0.1.3';
+const lunaVersion = 'v0.1.4';
 $(document).ready(function(){
   $(".version").each(function(){
       if($(this).is(":empty")){
           $(this).text(lunaVersion);
       }
   });
+});
+
+$('.a-close').click(function(e){
+  var alert = $(e.target).parent().parent();
+  alert.css('transform', 'translateY(-10px)').css('opacity', '0').delay(250).fadeOut(0);
 });
